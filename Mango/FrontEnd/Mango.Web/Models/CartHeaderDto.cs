d989d31a992b9e059650fcb9cf9f1dc7e862a35d
Vaibhav <vaibhavkumar5219@gmail.com>
2024-06-02 14:42:25 +0530

    Check UI created in Web Project

--- Mango/FrontEnd/Mango.Web/Models/CartHeaderDto.cs
+++ Mango/FrontEnd/Mango.Web/Models/CartHeaderDto.cs
@@ -10,9 +10,11 @@ namespace Mango.Web.Models.Dto
         public string? CouponCode { get; set; }
         public Double Discount { get; set; }
         public Double CartTotal { get; set; }
-        public string? FirstName {  get; set; }
-        public string? LastName {  get; set; }
+        [Required]
+        public string? Name {  get; set; }
+        [Required]
         public string? Phone {  get; set; }
+        [Required]
         public string? Email {  get; set; }
     }
 }
