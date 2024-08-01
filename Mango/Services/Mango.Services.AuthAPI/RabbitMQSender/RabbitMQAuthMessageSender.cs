aab1bf6f8baae8961f54da4d70aee6dccb881fe1
Vaibhav <vaibhavkumar5219@gmail.com>
2024-08-01 22:20:34 +0530

    API Gateway added using ocelot

---
+++ Mango/Services/Mango.Services.AuthAPI/RabbitMQSender/RabbitMQAuthMessageSender.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Mango.Services.AuthAPI.RabbitMQSender
+{
+    public class RabbitMQAuthMessageSender : IRabbitMQAuthMessageSender
+    {
+        private readonly string _hostName;
+        private readonly string _userName;
+        private readonly string _password;
+        private IConnection _connection;
+        public RabbitMQAuthMessageSender()
+        {
+            _hostName = "localhost";
+            _userName = "guest";
+            _password = "guest";
+        }
+        public async void SendMessage(object message, string queueName)
+        {
+            var factory =new ConnectionFactory
+            {
+                HostName = _hostName,
+                UserName = _userName,
+                Password = _password
+            };
+
+            _connection = await factory.CreateConnectionAsync();
+
+            using var channel = await _connection.CreateChannelAsync();
+            await channel.QueueBindAsync(queueName, exchange:"", routingKey:queueName);
+
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            await channel.BasicPublishAsync(exchange: "", routingKey: queueName, body:body);
+        }
+    }
+}
